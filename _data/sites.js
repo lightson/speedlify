b762f89cba7027e57e30d78187e0ee6e40e249c1
t <kid2000@gmail.com>
2020-07-02 14:01:54 +0100

    Update sites.js

--- _data/sites.js
+++ _data/sites.js
@@ -6,15 +6,7 @@ module.exports = {
 			freshChrome: "site",
 		},
 		urls: [
-			"https://www.netlify.com/",
-			"https://www.netlify.com/donation-matching/",
-			"https://www.netlify.com/products/build/plugins/",
-			"https://www.netlify.com/with/sitecore/",
-			"https://www.netlify.com/with/drupal/",
-			"https://www.netlify.com/with/wordpress/",
-			"https://www.netlify.com/webinar/a-drupal-journey-to-the-jamstack/",
-			"https://jamstackconf.com/",
-			"https://jamstackconf.com/virtual/",
+			"https://eatcomplete.co",
 		]
 	},
 	ssg: {
@@ -26,13 +18,8 @@ module.exports = {
 			freshChrome: "run"
 		},
 		urls: [
-			"https://www.11ty.dev/",
-			"https://www.gatsbyjs.org/",
-			"https://gohugo.io/",
-			"https://nextjs.org/",
-			"https://nuxtjs.org/",
-			// "https://vuepress.vuejs.org/", // kept timing out in lighthouse
-			"https://gridsome.org/",
+			//"https://www.11ty.dev/", //
+
 		]
 	}
 };
